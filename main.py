14d24360e310d98c9ce863ecd76054b6c8c09e05
KKD <kossstyan87@mail.ru>
2023-03-15 15:39:44 +0300

    Добавил  функцию для формирования вывода

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def get_file_extension(file_name):
     if "." not in file_name:
         raise ValueError("Невозможно определить расширение файла")
     return file_name.split(".")[-1]
+def print_file_extension(file_name):
+    try:
+        extension = get_file_extension(file_name)
+        print(extension)
+    except Exception as e:
+        print(e)
