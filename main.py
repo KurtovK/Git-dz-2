25fb6b676bb848f90459c3f62b9c556ce3564ecd
KKD <kossstyan87@mail.ru>
2023-03-15 15:38:20 +0300

    Добавил  функцию для определния файла с исключением

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def execute_application():
-    pass
-if __name__=="__main__":
-    execute_application()
+def get_file_extension(file_name):
+    if "." not in file_name:
+        raise ValueError("Невозможно определить расширение файла")
+    return file_name.split(".")[-1]
